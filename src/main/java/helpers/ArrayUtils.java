ed51664171482d5999c88828f46c14702005697d
ryadarakesh <ryada.rakesh@gmail.com>
2022-04-24 20:30:35 +0530

    Shift/rotate array lements by mentioned positions

---
+++ src/main/java/helpers/ArrayUtils.java
@@ -0,0 +1,18 @@
+package helpers;
+
+public class ArrayUtils {
+    public static void reverseElements(int[] array, int start, int end) {
+        while (start < end) {
+            int temp = array[start];
+            array[start] = array[end];
+            array[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    public static void printElements(int[] array) {
+        for (int ele: array) {
+            System.out.print(ele+"  ");
+        }
+    }
+}
---
+++ src/main/java/runners/ArrayRotateRunner.java
@@ -0,0 +1,24 @@
+package runners;
+
+import helpers.ArrayUtils;
+
+public class ArrayRotateRunner {
+    public static void main(String[] args) {
+        rotate(new int[]{1, 2, 3, 4, 5, 6, 7},3);
+    }
+
+    // Left rotate elements by mentioned positions
+    // First reverse entire array then reverse from start to len-shifts-1 and len-shifts to end
+    private static void rotate(int[] array, int posToShift) {
+        System.out.println("Before Rotation:");
+        ArrayUtils.printElements(array);
+        ArrayUtils.reverseElements(array, 0, array.length-1);
+
+        ArrayUtils.reverseElements(array,0, array.length-posToShift-1);
+
+        ArrayUtils.reverseElements(array,array.length-posToShift, array.length-1);
+
+        System.out.println("\nAfter Rotation of "+posToShift);
+        ArrayUtils.printElements(array);
+    }
+}
